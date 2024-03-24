71ca5ed2c64690e8484432c63cc2802029daa2c1
Daniel McAteer <danielmcateer@Daniels-MacBook-Pro.local>
2024-03-24 07:34:40 -0400

    remove langchain prompting and replace with str prompts

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import streamlit as st
 from langchain_core.tools import tool
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent 
-from langchain_openai import ChatOpenAI
+from openai import OpenAI
+from langchain.schema import SystemMessage
 
 st.title('Hermes 2 Pro on MLX - Tool Use')
 
 # Point to the local server
-llm = ChatOpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
+llm = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
 
 # Get the prompt to use - you can modify this!
 prompt = hub.pull("hwchase17/openai-tools-agent")
@@ -27,13 +28,15 @@ def exponentiate(base: int, exponent: int) -> int:
     "Exponentiate the base to the exponent power."
     return base**exponent
 
-tools = [multiply, add, exponentiate]
+system_message = "<|im_start|>system You are a function calling AI model named Hermes 2 Pro. You are provided with function signatures within <tools></tools> XML tags. You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug into functions. Here are the available tools: <tools> {'type': 'function', 'function': {'name': 'multiply', 'description': 'Multiply two integers together.', 'parameters': {'type': 'object', 'properties': {'first_int': {'type': 'integer'}, 'second_int': {'type': 'integer'}}, 'required': ['first_int', 'second_int']}}} {'type': 'function', 'function': {'name': 'add', 'description': 'Add two integers.', 'parameters': {'type': 'object', 'properties': {'first_int': {'type': 'integer'}, 'second_int': {'type': 'integer'}}, 'required': ['first_int', 'second_int']}}} {'type': 'function', 'function': {'name': 'exponentiate', 'description': 'Exponentiate the base to the exponent power.', 'parameters': {'type': 'object', 'properties': {'base': {'type': 'integer'}, 'exponent': {'type': 'integer'}}, 'required': ['base', 'exponent']}}} </tools> Use the following pydantic model json schema for each tool call you will make: {'title': 'FunctionCall', 'type': 'object', 'properties': {'arguments': {'title': 'Arguments', 'type': 'object'}, 'name': {'title': 'Name', 'type': 'string'}}, 'required': ['arguments', 'name']} For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows: <tool_call> {'arguments': <args-dict>, 'name': <function-name>}</tool_call> <|im_end|>"
 
-# Construct the OpenAI Tools agent
-agent = create_openai_tools_agent(llm, tools, prompt)
+# tools = [multiply, add, exponentiate]
 
-# Create an agent executor by passing in the agent and tools
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+# # Construct the OpenAI Tools agent
+# agent = create_openai_tools_agent(llm, tools, prompt)
+
+# # Create an agent executor by passing in the agent and tools
+# agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -55,8 +58,15 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         with st.spinner("Reasoning..."):
             try:
-                response = agent_executor.invoke({"input": prompt})
-                st.markdown(response)
+                stream = llm.chat.completions.create(
+                    model='hermes-2-pro',
+                    messages=[
+                        {"system": system_message, "role": m["role"], "content": m["content"]}
+                        for m in st.session_state.messages
+                    ],
+                    stream=True,
+                )
+                response = st.write_stream(stream)
                 st.session_state.messages.append({"role": "assistant", "content": response})
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
